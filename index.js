638701da8533f9ad41801e2c1d36b8c48766e7a1
rug-gieri <rafaelruggieri.ti@gmail.com>
2025-06-29 16:26:34 -0400

    desafio felipao concluído

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-//Sempre que sentir ConstantSourceNode, sonhe
+let xp = prompt("Qual sua quantidade de experiência?")
+let nome = prompt("Qual é o nome do heroi?");
+ 
+switch (true) {
+case (xp >= 0 && xp <= 1000):
+console.log(`O heroi de nome ${nome} é ferro`);
+break;
+case (xp >= 1001 && xp <= 2000):
+console.log(`O heroi de nome ${nome} está no nível Bronze`);
+break;
+case (xp >= 2.001 && xp <= 5000):
+console.log(`O heroi de nome ${nome} está no nível Prata`);
+break;
+case (xp >= 5001 && xp <= 7000):
+console.log(`O heroi de nome ${nome} está no nível Ouro`);
+break;
+case (xp >= 7001 && xp <= 8000):
+console.log(`O heroi de nome ${nome} está no nível Platina`);
+break;
+case (xp >= 8001 && xp <= 9000):
+console.log(`O heroi de nome ${nome} está no nível Ascendente`);
+break;
+case (xp >= 9001 && xp <= 10000):
+console.log(`O heroi de nome ${nome} está no nível Imortal`);
+break;
+case (xp >= 9001 && xp <= 10000):
+console.log(`O heroi de nome ${nome} está no nível Imortal`);
+break;
+case (xp >= 100001):
+console.log(`O heroi de nome ${nome} está no nível radiante`);
+break;
+} 
 
-console.log("vamo filhao vamo funcionar")
-
-console.log("Esse vai ser o melhor ano da minha vida")
